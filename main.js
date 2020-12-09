92d3ba4d499584c828616d7690baa31a63e0e089
Michael Edrei C. Miguel <50045211+Dreii06@users.noreply.github.com>
2020-12-09 16:31:31 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ function validateForm() {
 	var name = document.forms["feedbackForm"]["uname"].value;
 	var email = document.forms["feedbackForm"]["uemail"].value;
 	var mess = document.forms["feedbackForm"]["umess"].value;
-	if (name == "") {
+	if (name == "" && email == "" && mess == "") {
+		alert("Please enter all required information.");
+		return false;
+	}
+	else if (name == "") {
 		alert("Please enter your name.");
 		return false;
 	}
@@ -14,10 +18,8 @@ function validateForm() {
 		alert("Please enter your feedback about the website.");
 		return false;
 	}
-	else if (name == "" && email == "" && mess == "") {
-		alert("Please enter all required information.");
-	}
 	else {
 		alert("Thank you for sending us your feedback!");
+		return true;
 	}
 }
