da4c1f715c977d184b86fc650fda088f13d967cd
Michael Edrei C. Miguel <50045211+Dreii06@users.noreply.github.com>
2020-12-09 14:40:56 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,91 +1,91 @@
-<?php
-
-echo "<!DOCTYPE html>\n";
-echo "<html>\n";
-echo "<head>\n";
-echo "<link rel=\"stylesheet\" href=\"style.css\">\n";
-echo "	<title>Glory Guide</title>\n";
-echo "</head>\n";
-echo "<body>\n";
-echo "	<div class=\"top-nav\">\n";
-echo "		<ul>\n";
-echo "			<li><img src=\"logo.png\"/></li>\n";
-echo "			<li class=\"title\"><a href=\"index.html\">GLORY GUIDE</a></li>\n";
-echo "		</ul>\n";
-echo "		<ul class=\"feature\">\n";
-echo "			<li><a href=\"heroes.html\">HEROES</a></li>\n";
-echo "			<li><a href=\"tierlists.html\">TIER LISTS</a></li>\n";
-echo "			<li><a href=\"about.html\">ABOUT</a></li>\n";
-echo "			<li><a href=\"feedback.html\">HELP</a></li>\n";
-echo "		</ul>\n";
-echo "	</div>\n";
-echo "	<div class=\"tier-lists\">\n";
-echo "		<h2 id=\"text-tierlists\">LATEST HEROES</h2>\n";
-echo "		<div class=\"lists-container\">\n";
-echo "			<div>\n";
-echo "				<div>\n";
-echo "					<img src=\"benedetta.png\" class=\"hero-pic\">\n";
-echo "				</div>\n";
-echo "				<div class=\"desc\">\n";
-echo "					<h4>BENEDETTA</h4>\n";
-echo "					<span>Role: Assassin</span>\n";
-echo "				</div>\n";
-echo "				<div>\n";
-echo "					<img src=\"brody.png\" class=\"hero-pic\">\n";
-echo "				</div>\n";
-echo "				<div class=\"desc\">\n";
-echo "					<h4>BRODY</h4>\n";
-echo "					<span>Role: Marksman</span>\n";
-echo "				</div>\n";
-echo "				<div>\n";
-echo "					<img src=\"barats.png\" class=\"hero-pic\">\n";
-echo "				</div>\n";
-echo "				<div class=\"desc\">\n";
-echo "					<h4>BARATS</h4>\n";
-echo "					<span>Role: Tank/Fighter</span>\n";
-echo "				</div>\n";
-echo "			</div>\n";
-echo "			<div>\n";
-echo "				<div>\n";
-echo "					<img src=\"khaleed.png\" class=\"hero-pic\">\n";
-echo "				</div>\n";
-echo "				<div class=\"desc\">\n";
-echo "					<h4>KHALEED</h4>\n";
-echo "					<span>Role: Fighter</span>\n";
-echo "				</div>\n";
-echo "				<div>\n";
-echo "					<img src=\"yz.png\" class=\"hero-pic\">\n";
-echo "				</div>\n";
-echo "				<div class=\"desc\">\n";
-echo "					<h4>YU ZHONG</h4>\n";
-echo "					<span>Role: Fighter</span>\n";
-echo "				</div>\n";
-echo "				<div>\n";
-echo "					<img src=\"luoyi.png\" class=\"hero-pic\">\n";
-echo "				</div>\n";
-echo "				<div class=\"desc\">\n";
-echo "					<h4>LUO YI</h4>\n";
-echo "					<span>Role: Mage/Support</span>\n";
-echo "				</div>\n";
-echo "			</div>\n";
-echo "		</div>\n";
-echo "	</div>\n";
-echo "	<div class=\"info\">\n";
-echo "		<h3 class=\"info-title\">THESE CAN HELP YOU REACH THE TOP!</h3>\n";
-echo "		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin, ligula ornare commodo tristique, enim purus imperdiet diam, a ultricies nibh lectus in risus. Maecenas lobortis sollicitudin mi sit amet luctus. Curabitur vitae cursus lacus. Nullam id metus eget mauris finibus porta at vel turpis. Praesent at diam bibendum justo auctor suscipit quis sollicitudin tortor.</p>\n";
-echo "		<button class=\"btn-create-tierlists\">VIEW TIER LISTS</button>\n";
-echo "	</div>\n";
-echo "	<div class=\"bot-nav\">\n";
-echo "		<div class=\"about\">\n";
-echo "			<span>2020 GLORYGUIDES.COM - ALL RIGHTS RESERVED</span>\n";
-echo "		</div>\n";
-echo "		<div class=\"socials\">\n";
-echo "			<button class=\"btnfacebook\" type=\"facebook\"><img src=\"facebook.png\" width=\"25\" height=\"25\"></button>\n";
-echo "			<button class=\"btntwitter\" type=\"twitter\"><img src=\"twitter.png\" width=\"25\" height=\"25\"></button>\n";
-echo "			<button class=\"btndiscord\" type=\"discord\"><img src=\"discord.png\" width=\"25\" height=\"25\"></button>\n";
-echo "		</div>\n";
-echo "	</div>\n";
-echo "</body>\n";
-echo "</html>";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+	<title>Glory Guide</title>
+</head>
+<body>
+	<div class="top-nav">
+		<ul>
+			<li><img src="logo.png"/></li>
+			<li class="title"><a href="index.html">GLORY GUIDE</a></li>
+		</ul>
+		<ul class="feature">
+			<li><a href="heroes.html">HEROES</a></li>
+			<li><a href="tierlists.html">TIER LISTS</a></li>
+			<li><a href="about.html">ABOUT</a></li>
+			<li><a href="feedback.html">HELP</a></li>
+		</ul>
+	</div>
+	<div class="tier-lists">
+		<h2 id="text-tierlists">LATEST HEROES</h2>
+		<div class="lists-container">
+			<?php
+			$newheroes = array("BENEDETTA", "BRODY", "BARATS", "KHALEED", "YU ZHONG", "LUO YI");
+			$roles = array("Assassin", "Marksman", "Tank", "Support", "Mage", "Fighter")
+			<div>
+				<div>
+					<img src="benedetta.png" class="hero-pic">
+				</div>
+				<div class="desc">
+					echo "<h4>" . $newheroes[0] . "</h4>";
+					<span>Role: Assassin</span>
+				</div>
+				<div>
+					<img src="brody.png" class="hero-pic">
+				</div>
+				<div class="desc">
+					<h4>BRODY</h4>
+					<span>Role: Marksman</span>
+				</div>
+				<div>
+					<img src="barats.png" class="hero-pic">
+				</div>
+				<div class="desc">
+					<h4>BARATS</h4>
+					<span>Role: Tank/Fighter</span>
+				</div>
+			</div>
+			<div>
+				<div>
+					<img src="khaleed.png" class="hero-pic">
+				</div>
+				<div class="desc">
+					<h4>KHALEED</h4>
+					<span>Role: Fighter</span>
+				</div>
+				<div>
+					<img src="yz.png" class="hero-pic">
+				</div>
+				<div class="desc">
+					<h4>YU ZHONG</h4>
+					<span>Role: Fighter</span>
+				</div>
+				<div>
+					<img src="luoyi.png" class="hero-pic">
+				</div>
+				<div class="desc">
+					<h4>LUO YI</h4>
+					<span>Role: Mage/Support</span>
+				</div>
+			</div>
+			?>
+		</div>
+	</div>
+	<div class="info">
+		<h3 class="info-title">THESE CAN HELP YOU REACH THE TOP!</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin, ligula ornare commodo tristique, enim purus imperdiet diam, a ultricies nibh lectus in risus. Maecenas lobortis sollicitudin mi sit amet luctus. Curabitur vitae cursus lacus. Nullam id metus eget mauris finibus porta at vel turpis. Praesent at diam bibendum justo auctor suscipit quis sollicitudin tortor.</p>
+		<button class="btn-create-tierlists">VIEW TIER LISTS</button>
+	</div>
+	<div class="bot-nav">
+		<div class="about">
+			<span>2020 GLORYGUIDES.COM - ALL RIGHTS RESERVED</span>
+		</div>
+		<div class="socials">
+			<button class="btnfacebook" type="facebook"><img src="facebook.png" width="25" height="25"></button>
+			<button class="btntwitter" type="twitter"><img src="twitter.png" width="25" height="25"></button>
+			<button class="btndiscord" type="discord"><img src="discord.png" width="25" height="25"></button>
+		</div>
+	</div>
+</body>
+</html>
