2c8a845fbd2b00d467939ea39643775e559eaa50
Michael Edrei C. Miguel <50045211+Dreii06@users.noreply.github.com>
2020-12-18 17:55:20 +0800

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,16 +1,16 @@
 <?php
 
-$uname = $_REQUEST['uname'];
-$uemail = $_REQUEST['uemail'];
-$umess = $_REQUEST['umess'];
+$name = $_REQUEST['uname'];
+$email = $_REQUEST['uemail'];
+$mess = $_REQUEST['umess'];
 
-if (empty($uname) || empty($uemail) || empty($umess)) {
+if (empty($name) || empty($email) || empty($mess)) {
 	echo('Please enter all required information');
 }
 else {
-	mail("mecm06.mm@gmail.com", "Feedback Message", $umess, "From: $name <$email>");
+	mail("mecm06.mm@gmail.com", "Feedback Message", $mess, "From: $name <$email>");
 	echo "<script type='text/javascript'>alert('Thank you for sending us your feedback!')
-		window.history.log(-1);
-	</script>";
+		window.history.go(-1);
+	</script>";	
 }
 ?>
